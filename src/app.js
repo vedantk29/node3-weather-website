9fce83cfe7981a302df94a8a425be26cb363c7b4
Akash Kachhiya <akashkachhiya@Akashs-Mini-2.ho.dhru.io>
2023-05-03 16:11:40 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-
+const port = process.env.PORT || 3000
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname, '../templates')
