f57f0ca49527423693f5c4d194335c30cb192e9d
Akash Kachhiya <akashkachhiya@Akashs-Mini-2.ho.dhru.io>
2023-05-04 12:12:24 +0530

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request')
 
+
+
 const forecast = (latitude, longitude, callback) => {
    const url = 'https://api.darksky.net/forecast/db1928b8e8ec6c5cb1b3f98e8da404a3&units=f' + latitude + ',' + longitude
 
@@ -9,7 +11,8 @@ request ( {url,  json: true}, (error, {body } ) => {
         } else if (response.body.error) {
            callback('Unable to find location', undefined  )
         } else {
-            callback(undifined, body.daily.data[0]. summary + 'It is currently ' + body.currently.temperature + 'degress out. There is a ' + body.currently.precipProbability)  
+             console.log(body.daily.data[0])
+            callback(undifined, body.daily.data[0]. summary + 'It is currently ' + body.currently.temperature + 'degress out. This high today is' +body.daily.data[0].temperatureHigh +'with a low of'+ body.daily.data [0]. temperaturelow +' . There is a'  + body.currently.precipProbability)  
         }
    } )
 }
